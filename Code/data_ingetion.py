c31346ee863ad38c6719c7d4e7120aa8d954542c
Yash Kavaiya <yash.kavaiya3@gmail.com>
2024-08-24 11:00:48 +0000

    LANGCHAIN_TRACING_V2

---
+++ Code/data_ingetion.py
@@ -0,0 +1,9 @@
+from langchain_community.document_loaders import YoutubeLoader
+
+
+LANGCHAIN_TRACING_V2=true
+LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
+LANGCHAIN_API_KEY="LANGCHAIN_API_KEY"
+LANGCHAIN_PROJECT="Youtube-RAG"
+
+
